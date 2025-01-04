acfbd535121681dd4c38be82eb33940819f8469a
Jing Hua <tohjinghua123@gmail.com>
2025-01-04 22:55:24 +0000

    add train baseline script

---
+++ scripts_mine/train_baseline.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+### TC1 Job Script ###
+ 
+#SBATCH --partition=UGGPU-TC1
+#SBATCH --qos=normal
+#SBATCH --gres=gpu:1
+
+### Specify Memory allocate to this job ###
+#SBATCH --mem=64G
+
+### Specify number of core (CPU) to allocate to per node ###
+#SBATCH --ntasks-per-node=1
+
+#SBATCH --cpus-per-task=20 
+
+### Specify number of node to compute ###
+#SBATCH --nodes=1
+
+### Optional: Specify node to execute the job ###
+### Remove 1st # at next line for the option to take effect ###
+##SBATCH --nodelist=TC1N07
+
+### Specify Time Limit, format: <min> or <min>:<sec> or <hr>:<min>:<sec> or <days>-<hr>:<min>:<sec> or <days>-<hr> ### 
+#SBATCH --time=360
+
+### Specify name for the job, filename format for output and error ###
+#SBATCH --job-name=train_baseline
+#SBATCH --output=/home/FYP/tohj0037/spoc-robot-training/logs/output_%x_%j.out
+#SBATCH --error=/home/FYP/tohj0037/spoc-robot-training/logs/error_%x_%j.err
+
+### Your script for computation ###
+source /home/FYP/tohj0037/miniconda3/bin/activate
+conda activate spoc
+
+export VULKAN_SDK=/home/FYP/tohj0037/vulkan-sdk/x86_64
+export PATH=$VULKAN_SDK/bin:$PATH
+export LD_LIBRARY_PATH=$VULKAN_SDK/lib:$LD_LIBRARY_PATH
+export VK_LAYER_PATH=$VULKAN_SDK/share/vulkan/explicit_layer.d
+export VK_ADD_LAYER_PATH=$VULKAN_SDK/share/vulkan/explicit_layer.d
+
+export WANDB_API_KEY="<WANDB_API_KEY>"
+export WANDB_NOTES="Train baseline"
+
+export OBJAVERSE_DATA_BASE_DIR="objaverse_assets"
+export OBJAVERSE_HOUSES_BASE_DIR="objaverse_houses"
+export OBJAVERSE_DATA_DIR="objaverse_assets/2023_07_28"
+export OBJAVERSE_HOUSES_DIR="objaverse_houses/houses_2023_07_28"
+export PYTHONPATH="./"
+
+cd /home/FYP/tohj0037/spoc-robot-training
+
+
+python -m training.offline.train_pl \
+  --max_samples 459816 \
+  --eval_max_samples 100 \
+  --eval_every 50 \
+  --save_every 2000 \
+  --model_version siglip_base_3_double_det \
+  --sliding_window 100 \
+  --per_gpu_batch 3 \
+  --lr 0.0001 \
+  --data_dir /home/FYP/tohj0037/spoc-robot-training/data/all \
+  --dataset_version OBJECTNAV \
+  --model EarlyFusionCnnTransformer \
+  --input_sensors raw_navigation_camera raw_manipulation_camera last_actions an_object_is_in_hand \
+ nav_task_relevant_object_bbox manip_task_relevant_object_bbox nav_accurate_object_bbox manip_accurate_object_bbox \
+  --precision 16-mixed \
+  --loss action \
+  --max_epochs 400 \
+  --output_dir /home/FYP/tohj0037/spoc-robot-training/train_logs \
+  --num_nodes 1 \
+  --wandb_logging True \
+  --wandb_entity_name jinghua \
+  --wandb_project_name fyp_spoc \
+  --resume \
+  --run_id otq3k54w \
+  --step 12000
